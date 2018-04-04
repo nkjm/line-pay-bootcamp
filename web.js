c0d5e1cf406db7db9ae59c0c1503f5ef0d839a9d
Kazuki Nakajima <kazuki.nakajima@linecorp.com>
2018-04-04 22:31:19 +0900

    Add LINE Pay to web app

--- web.js
+++ web.js
@@ -19,3 +19,72 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
     res.render(__dirname + "/index");
 })
+
+// Import environment variables from .env file.
+require("dotenv").config();
+
+// Import packages.
+const uuid = require("uuid/v4");
+const cache = require("memory-cache");
+
+// Instanticate LINE Pay API SDK.
+const line_pay = require("line-pay");
+const pay = new line_pay({
+    channelId: process.env.LINE_PAY_CHANNEL_ID,
+    channelSecret: process.env.LINE_PAY_CHANNEL_SECRET,
+    hostname: process.env.LINE_PAY_HOSTNAME,
+    isSandbox: true
+})
+
+// Router configuration to start payment.
+app.use("/pay/reserve", (req, res) => {
+    let options = {
+        productName: "チョコレート",
+        amount: 1,
+        currency: "JPY",
+        orderId: uuid(),
+        confirmUrl: process.env.LINE_PAY_CONFIRM_URL
+    }
+
+    pay.reserve(options).then((response) => {
+        let reservation = options;
+        reservation.transactionId = response.info.transactionId;
+
+        console.log(`Reservation was made. Detail is following.`);
+        console.log(reservation);
+
+        // Save order information
+        cache.put(reservation.transactionId, reservation);
+
+        res.redirect(response.info.paymentUrl.web);
+    })
+})
+
+// Router configuration to recieve notification when user approves payment.
+app.use("/pay/confirm", (req, res) => {
+    if (!req.query.transactionId){
+        throw new Error("Transaction Id not found.");
+    }
+
+    // Retrieve the reservation from database.
+    let reservation = cache.get(req.query.transactionId);
+    if (!reservation){
+        throw new Error("Reservation not found.");
+    }
+
+    console.log(`Retrieved following reservation.`);
+    console.log(reservation);
+
+    let confirmation = {
+        transactionId: req.query.transactionId,
+        amount: reservation.amount,
+        currency: reservation.currency
+    }
+
+    console.log(`Going to confirm payment with following options.`);
+    console.log(confirmation);
+
+    pay.confirm(confirmation).then((response) => {
+        res.send("決済が完了しました。");
+    });
+})
